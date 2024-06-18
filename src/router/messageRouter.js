e7876007e38cc7662c1ff57d90173a6e7307e8cf
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-06-18 15:36:31 +0530

    feat: Parsed Url using 'new URL(request.url, http://${request.headers.host}', Store userId inside ws connection(eg: ws.userId), create a clientsMap 'Map' to store userId with the ws connection of that user as key-value pair. store the 'newMessage' to the database based on that the receiver's ws connection is active or not by checking receiver_Id inside that clientsMap collection if receiver isn't active then 'newMessage.state' should be default 'pending', If algo able to find the receiver's connection in clientMap then I'm changing the 'newMessage.state' to 'sent' then after that save the 'newMessage' in database. And when the user get disconnected I remove the user ws connection from the 'clientMap' using ws.userId

--- src/router/messageRouter.js
+++ src/router/messageRouter.js
@@ -6,39 +6,72 @@ const messageRouter = Router();
 
 const wss = new WebSocketServer({ noServer: true });
 
-wss.on('connection', (ws) => {
+const clientsMap = new Map();
+
+wss.on('connection', (ws, request) => {
     console.log('Client connected to WS');
+    
+    console.log(clientsMap.size);
+    const parsedUrl = new URL(request.url, `http://${request.headers.host}`);
+    ws.userId = parsedUrl.searchParams.get('userId');
+    
+    if (ws.userId) {
+        clientsMap.set(ws.userId, ws);
+        console.log(`User ${ws.userId} connected`)
+        console.log(clientsMap.size);
 
-    ws.on('message', async (message, isBinary) => {
+        ws.on('message', async (message, isBinary) => {
 
-        const { sender_id, receiver_id, content } = JSON.parse(message);
-        try {
-            const newMessage = new Message({
-                sender_id,
-                receiver_id,
-                content
-            });
+            const { sender_id, receiver_id, content, state } = JSON.parse(message);
+            try {
 
-            await newMessage.save();
+                const senderWs = clientsMap.get(sender_id)
+                const receiverWs = clientsMap.get(receiver_id)
+                let newMessage = new Message({
+                    sender_id,
+                    receiver_id,
+                    content,
+                    state
+                });
 
-            wss.clients.forEach((client) =>  {
-                if (client.readyState === WebSocket.OPEN) {
-                    client.send(JSON.stringify({ event: 'messageSaved', message: newMessage }), { binary: isBinary });
+                // add Redis Queue logic ///////////////////////////////////
+
+                if (receiverWs && receiverWs.readyState === WebSocket.OPEN) {
+                    newMessage.state = 'sent'
+                    await newMessage.save();
+                    receiverWs.send(JSON.stringify({ event: 'messageSaved', message: newMessage }), { binary: isBinary });
+                    senderWs.send(JSON.stringify({ event: 'messageSaved', message: newMessage }), { binary: isBinary });
+                } else if (!receiverWs) {
+                    await newMessage.save();
+                    senderWs.send(JSON.stringify({ event: 'messageSaved', message: newMessage }), { binary: isBinary });
                 }
-            });
-            // In a real application, I need to have a mechanism to find and notify the receiver
-            // Here, we are just logging for simplicity
-            console.log(`Message from ${sender_id} to ${receiver_id}: ${content}`);
 
-        } catch (error) {
-            console.error('Error saving message:', error);
-            ws.send(JSON.stringify({ event: 'error', message: 'Message could not be saved. Please try again.' }));
-        }
-    });
+                console.log(`Message from ${sender_id} to ${receiver_id}: ${content} is pending`);
+                // ROOM LOGIC
+                // wss.clients.forEach((client) => {
+                //     if (client.readyState === WebSocket.OPEN) {
+                //         client.send(JSON.stringify({ event: 'messageSaved', message: newMessage }), { binary: isBinary });
+                //     }
+                // });
 
-    ws.on('close', () => {
-        console.log('Client disconnected');
-    });
+                // In a real application, I need to have a mechanism to find and notify the receiver
+                // Here, we are just logging for simplicity
+
+            } catch (error) {
+                console.error('Error handling message:', error);
+                ws.send(JSON.stringify({ event: 'error', message: 'Message could not be saved. Please try again.' }));
+            }
+        });
+        ws.on('close', () => {
+            console.log('Client disconnected');
+            clientsMap.delete(ws.userId);
+            console.log(clientsMap.size);
+        });
+    } else {
+        console.log(clientsMap.size);
+        console.error('Connection attempt without userId');
+        ws.close();
+    }
 });
 
 messageRouter.get('/chat', (req, res) => {
