dbbeac0faaab87424ada721ac362b2a0e8139c0d
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-07-20 12:54:59 +0530

    feat(auth.js): create custom Authorization function verify the jsonwebtoken and find the user with decoded object from the token and set req.user and req.token

---
+++ src/middleware/auth.js
@@ -0,0 +1,25 @@
+import jwt from "jsonwebtoken";
+import User from "../models/user.js";
+
+const auth = async (req, res, next) => {
+    try {
+        const token = req.header("Authorization").replace("Bearer ", "");
+
+        if (!token) {
+            throw new Error();
+        }
+        
+        const decoded = jwt.verify(token, "This is a temporary Private Key");
+        const user = User.findOne({username: decoded.username, _id: decoded._id, 'tokens.token': token});
+        if(!user) {
+            throw new Error();
+        }
+        req.token = token;
+        req.user = user;
+        next()
+    } catch (error) {
+        res.status(401).send({ error: 'Please Authenticate' })
+    }
+}
+
+export default auth;
