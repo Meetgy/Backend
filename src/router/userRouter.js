9edf25084c310378ce5b40492a2a5b46c37b1bd4
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-07-21 05:21:58 +0530

    feat(userRouter.js): Implement generateAuthToken with 'signup' post request & modify user name from req.body with '@'

--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -4,11 +4,15 @@ import User from "../models/user.js";
 const userRouter = new Router();
 
 userRouter.post('/signup', async (req, res) => {
+    if(!req.body.username.includes('@')) {
+        req.body.username = `@${req.body.username}`
+    }
     const user = new User(req.body);
 
     try {
         await user.save();
-        res.status(200).send(user)
+        const token = await user.generateAuthToken();
+        res.status(200).send({ user, token });
     } catch (error) {
         res.status(400).json(error)
     }
